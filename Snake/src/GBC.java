4467e0d61c9833bba4bd2b8228b960ada7780c0f
itsfr <itsfr@JEANNE>
2023-12-10 22:30:42 +0100

    Despues de 2 dias a full lo tenemos

--- Snake/src/GBC.java
+++ Snake/src/GBC.java
@@ -16,36 +16,4 @@ public class GBC extends GridBagConstraints {
         this.gridwidth = gridwidth;
         this.gridheight = gridheight;
     }
-
-    public GBC setAnchor(int anchor) {
-        this.anchor = anchor;
-        return this;
-    }
-
-    public GBC setFill(int fill) {
-        this.fill = fill;
-        return this;
-    }
-
-    public GBC setWeight(double weightx, double weighty) {
-        this.weightx = weightx;
-        this.weighty = weighty;
-        return this;
-    }
-
-    public GBC setInsets(int distance) {
-        this.insets = new Insets(distance, distance, distance, distance);
-        return this;
-    }
-
-    public GBC setInsets(int top, int left, int bottom, int right) {
-        this.insets = new Insets(top, left, bottom, right);
-        return this;
-    }
-
-    public GBC setIpad(int ipadx, int ipady) {
-        this.ipadx = ipadx;
-        this.ipady = ipady;
-        return this;
-    }
 }
